9d72052810dcb6b1592c98836e0d3cfb9c64584c
Andreas Isnes <andreas.isnes@gmail.com>
2026-06-01 15:10:20 +0200

    chore: initial commit

--- flake.nix
+++ flake.nix
@@ -4,14 +4,20 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }: {
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    nixosConfigurations.server = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./hosts/server
+        home-manager.nixosModules.home-manager
+      ];
     };
+  };
 }
