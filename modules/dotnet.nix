fbf35570f662928aba81025bd75208df47cf9dfc
Andreas Isnes <andreas.isnes@gmail.com>
2026-06-01 21:38:25 +0200

    chore: initial commit

--- modules/dotnet.nix
+++ modules/dotnet.nix
@@ -4,7 +4,7 @@
   environment.systemPackages = with pkgs; [
     dotnet-sdk_9
     dotnet-sdk_10
-    dotnet_aspnetcore_9
+    dotnet-aspnetcore_9
     dotnet-aspnetcore_10
     dotnet-runtime_9
     dotnet-runtime_10
