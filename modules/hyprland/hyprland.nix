420e4b1f284ea3d90e8d128df3af1e2016394963
Andreas Isnes <andreas.isnes@gmail.com>
2026-06-01 23:04:19 +0200

    add login manager

--- modules/hyprland/hyprland.nix
+++ modules/hyprland/hyprland.nix
@@ -1,6 +1,10 @@
 { pkgs, ... }:
 {
-  programs.hyprland.enable = true; # system-wide, needed for session in tuigreet
+  programs.hyprland = {
+    enable = true; # system-wide, needed for session in tuigreet
+    withUWSM = true; # recommended for most users
+    xwayland.enable = true; # Xwayland can be disabled
+  };
   nix.settings = {
     substituters = [ "https://hyprland.cachix.org" ];
     trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
